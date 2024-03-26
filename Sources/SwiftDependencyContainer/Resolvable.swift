2509908de6d4f0803dfaebd6d6f6ed12ab689888
David Scheutz <scheutz.david+github@gmail.com>
2024-03-25 21:00:11 -0500

    extend Resolvable functionality

--- Sources/SwiftDependencyContainer/Resolvable.swift
+++ Sources/SwiftDependencyContainer/Resolvable.swift
@@ -3,3 +3,17 @@ import Foundation
 public protocol Resolvable {
     func resolve<T>() -> T
 }
+
+internal struct DependencyResolver: Resolvable {
+    let container: DependencyContainer
+    
+    func resolve<T>() -> T {
+        try! container.resolve()
+    }
+}
+
+extension DependencyContainer {
+    public func resolver() -> Resolvable {
+        DependencyResolver(container: self)
+    }
+}
