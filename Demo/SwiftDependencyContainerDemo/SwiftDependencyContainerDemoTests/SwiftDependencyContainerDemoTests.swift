0d818825256182bc2f4acb09a4b2434c244759ac
David Scheutz <scheutz.david+github@gmail.com>
2024-03-27 22:43:12 -0500

    add functionality to resolve every registered Singleton; update tests

--- Demo/SwiftDependencyContainerDemo/SwiftDependencyContainerDemoTests/SwiftDependencyContainerDemoTests.swift
+++ Demo/SwiftDependencyContainerDemo/SwiftDependencyContainerDemoTests/SwiftDependencyContainerDemoTests.swift
@@ -3,22 +3,7 @@ import XCTest
 
 final class SwiftDependencyContainerDemoTests: XCTestCase {
 
-    func test_dependenciesCodeGen() throws {
-        // TODO: generate test function that resolves all dependencies
-//        try Dependencies.test()
-        
-        let apiClient = Dependencies.api
-        let authApi = Dependencies.authApi
-        let userApi = Dependencies.userApi
-        
-        // TODO: figure out how to verify variable type
-//        XCTAssertEqual(variableType(authApi), AuthApi.self)
-//        XCTAssertEqual(variableType(userApi), UserApi.self)
-        
-        // Verify same instance
-        XCTAssertTrue(apiClient === userApi)
-        XCTAssertTrue(userApi === authApi)
-        XCTAssertTrue(authApi === ApiClient.resolveAuthApi())
-        XCTAssertTrue(userApi === ApiClient.resolveUserApi())
+    func test_resolveRegisteredSingletons() throws {
+        try Dependencies.verifyResolvability()
     }
 }
